c66c62b4f289d6b8cb12e82861121aa7eda85ea2
Leslie Osorio <lesosorio17@gmail.com>
2018-10-22 18:38:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 
-def reverse_each_word(sentence1)
-  sentence1.each do |sentence1|
-    puts "Hello there, and how are you?"
-    sentence1.reverse
-end
-end
+def reverse_each_word("Hello there, and how are you?")
+  "Hello there, and how are you?".reverse
+ end 
+end 
+  
